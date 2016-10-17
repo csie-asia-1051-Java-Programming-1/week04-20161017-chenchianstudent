ea5210f28101813097398dded0680fa06a6e4778
chenchianstudent <105021007@live.asia.edu.tw>
2016-10-17 21:06:20 +0800

    hw end

--- src/hw/hw01_105021007.java
+++ src/hw/hw01_105021007.java
@@ -9,17 +9,20 @@ public class hw01_105021007 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn=new Scanner(System.in);
-		int v1=scn.nextInt();
-		int v2=0;
+		int n=scn.nextInt();
+		int v2=1;
 		int v3=0;
+		int v1=0;
+		int v5=1;
 		
-		
-		for(int i=0;i<10;i++){
+		System.out.print(v5+"\t"+v5);
+		for(int i=0;i<n;i++){
 			v3=v2+v1;
 			v1=v2;
 			v2=v3;
+			v3=v1+v2;
 			
-			System.out.print(v3);
+			System.out.print("\t"+v3);
 			
 			}
 		
